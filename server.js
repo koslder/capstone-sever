b9d9b3dc448ae21e22eb717ea9f498698f568311
koslder <franciscoecojedo2002@gmail.com>
2024-12-22 14:48:12 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.error(err));
+
+// Routes
+app.get('/', (req, res) => res.send('API is running'));
+
+// Port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+console.log("hello");
