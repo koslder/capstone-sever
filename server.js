6a480e8a720fb350090d325a1886583be768c971
koslder <franciscoecojedo2002@gmail.com>
2024-12-22 15:08:51 +0800

    test2

--- server.js
+++ server.js
@@ -18,10 +18,8 @@ mongoose.connect(process.env.MONGO_URI, {
     .catch((err) => console.error(err));
 
 // Routes
-app.get('/', (req, res) => res.send('API is running'));
+app.get('/', (req, res) => res.send('API is running Hello World'));
 
 // Port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-console.log("hello");
