44bd388ff36d8e8bae850b8a1f283467fb2f9394
koslder <franciscoecojedo2002@gmail.com>
2024-12-28 02:30:13 +0800

    Creating API Test: 1

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import Admin from './models/admin';
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -20,6 +22,24 @@ app.use('/api', userRoutes);  // Set up API endpoint prefix
 
 // Basic test route to verify if the server is running
 app.get('/', (req, res) => res.send('Hello, World!'));
+
+app.post('/api/admin', async (req, res) => {
+    const admin = req.body;
+
+    if (!admin.email || !admin.username || !admin.password) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
+    }
+
+    const newAdmin = new Admin(admin);
+
+    try {
+        await newAdmin.save();
+        res.status(202).json({ success: true, message: "New Admin has been created!" });
+    } catch (error) {
+        console.log("Error on creating new Admin!");
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+})
 
 // Start server
 const PORT = process.env.PORT || 5000;
