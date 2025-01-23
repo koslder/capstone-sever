21a93876e60448d70555a99bb390898dad430fbe
koslder <franciscoecojedo2002@gmail.com>
2025-01-23 16:00:47 +0800

    1-23-2025

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -44,11 +44,13 @@ const createEvent = async (req, res) => {
 
 const getAllEvents = async (req, res) => {
     try {
-        const events = await Event.find().populate('aircon');
+        const events = await Event.find()
+            .populate('aircon') // Populate Aircon details
+            .populate('technicians', 'firstname lastname'); // Populate specific fields for technicians
         res.status(200).json(events);
     } catch (error) {
-        console.error('Error fetching events:', error);
-        res.status(500).json({ message: 'Error fetching events' });
+        console.error('Error fetching events:', error.message);
+        res.status(500).json({ message: error.message });
     }
 };
 
